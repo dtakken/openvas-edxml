b8c4b4c2f825349af64130d7606f3770e30caaa8
Dik Takken <d.h.j.takken@freedom.nl>
2021-05-12 17:37:03 +0200

    Various improvements to the OpenVAS ontology brick

--- openvas_edxml/brick.py
+++ openvas_edxml/brick.py
@@ -13,8 +13,8 @@ class OpenVASBrick(Brick):
     OBJECT_ERROR_MESSAGE = 'org.openvas.error-message'
     OBJECT_NVT_FAMILY = 'org.openvas.nvt.family'
     OBJECT_SCAN_NAME = 'org.openvas.scan.name'
-    OBJECT_QOD_TYPE = 'org.openvas.result.qod.type'
-    OBJECT_QOD_VALUE = 'org.openvas.result.qod.value'
+    OBJECT_QOD_TYPE = 'org.openvas.result.detection.type'
+    OBJECT_QOD_VALUE = 'org.openvas.result.detection.quality'
     OBJECT_SEVERITY = 'org.openvas.result.severity'
     OBJECT_THREAT = 'org.openvas.result.threat'
     OBJECT_IMPACT = 'org.openvas.result.impact'
@@ -45,66 +45,70 @@ class OpenVASBrick(Brick):
     def generate_object_types(cls, target_ontology):
 
         yield target_ontology.create_object_type(cls.OBJECT_NVT_NAME) \
-            .set_description('name of an OpenVAS plugin (NVT)')\
+            .set_description('a name of an OpenVAS plugin (NVT)')\
             .set_data_type(DataType.string(255))\
             .set_display_name('plugin name')
 
         yield target_ontology.create_object_type(cls.OBJECT_ERROR_MESSAGE) \
-            .set_description('error message produced by an OpenVAS plugin (NVT)')\
+            .set_description('an error message produced by an OpenVAS plugin (NVT)')\
             .set_data_type(DataType.string(255))\
             .set_display_name('error message')
 
         yield target_ontology.create_object_type(cls.OBJECT_NVT_FAMILY) \
-            .set_description('name of a category of OpenVAS plugins')\
+            .set_description('a name of a category of OpenVAS plugins')\
             .set_data_type(DataType.string(255))\
             .set_display_name('plugin family', 'plugin families')
 
         yield target_ontology.create_object_type(cls.OBJECT_SCAN_NAME) \
-            .set_description('name of an OpenVAS scan')\
+            .set_description('a name of an OpenVAS scan')\
             .set_data_type(DataType.string(255))\
             .set_display_name('scan name')
 
         yield target_ontology.create_object_type(cls.OBJECT_QOD_TYPE) \
-            .set_description('OpenVAS detection reliability indicator')\
+            .set_description('an OpenVAS detection reliability indicator')\
             .set_data_type(DataType.enum('other', *cls.KNOWN_QOD_TYPES))\
             .set_display_name('QoD type')
 
         yield target_ontology.create_object_type(cls.OBJECT_QOD_VALUE) \
-            .set_description('OpenVAS detection reliability value, in percent')\
+            .set_description('an OpenVAS detection reliability value, in percent')\
             .set_data_type(DataType.tiny_int(signed=False))\
+            .set_unit('percent', '%')\
             .set_display_name('QoD value')
 
         yield target_ontology.create_object_type(cls.OBJECT_SEVERITY) \
-            .set_description('severity of an OpenVAS detection result')\
+            .set_description('a severity of an OpenVAS detection result')\
             .set_data_type(DataType.decimal(total_digits=3, fractional_digits=1))\
             .set_display_name('vulnerability severity', 'vulnerability severities')
 
         yield target_ontology.create_object_type(cls.OBJECT_THREAT) \
-            .set_description('threat level of an OpenVAS detection result')\
+            .set_description('a threat level of an OpenVAS detection result')\
             .set_data_type(DataType.enum('high', 'medium', 'low', 'alarm', 'log', 'debug'))\
             .set_display_name('threat level')
 
         yield target_ontology.create_object_type(cls.OBJECT_IMPACT) \
-            .set_description('description of the impact of an issue detected by OpenVAS')\
+            .set_description('a description of the impact of an issue detected by OpenVAS')\
             .set_data_type(DataType.string())\
-            .set_display_name('impact description')
+            .set_display_name('impact description')\
+            .compress()
 
         yield target_ontology.create_object_type(cls.OBJECT_INSIGHT)\
-            .set_description('technical details about an issue detected by OpenVAS')\
+            .set_description('a technical detail about an issue detected by OpenVAS')\
             .set_data_type(DataType.string())\
-            .set_display_name('issue detail')
+            .set_display_name('issue detail')\
+            .compress()
 
         yield target_ontology.create_object_type(cls.OBJECT_SOLUTION_TYPE)\
-            .set_description('type of solution for an issue detected by OpenVAS')\
+            .set_description('a type of solution for an issue detected by OpenVAS')\
             .set_data_type(DataType.string(255))\
-            .set_display_name('solution type')
+            .set_display_name('solution type')\
+            .set_regex_soft('Workaround|Mitigation|Vendor-Fix|None-Available|WillNotFix')
 
     @classmethod
     def generate_concepts(cls, target_ontology):
 
         yield target_ontology.create_concept(cls.CONCEPT_FINDING) \
             .set_display_name('OpenVAS finding') \
-            .set_description('OpenVAS detection result')
+            .set_description('an OpenVAS detection result')
 
 
 edxml.ontology.Ontology.register_brick(OpenVASBrick)
