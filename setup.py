edad4e37b8655dc0b3a5d8d6e824c812a857c134
Dik Takken <d.h.j.takken@freedom.nl>
2021-08-16 17:06:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,15 +10,39 @@ setup(
     version=__version__,
 
     # A description of your project
-    description='An OpenVAS XML to EDXML transcoder library',
+    description='An OpenVAS XML to EDXML transcoder',
     long_description='EDXML transcoder that takes OpenVAS XML reports as input and outputs EDXML data',
+
+    # The project's main homepage
+    url='https://github.com/dtakken/edxml-openvas',
 
     # Author details
     author='Dik Takken',
-    author_email='dik.takken@northwave.nl',
+    author_email='d.h.j.takken@freedom.nl',
 
     # Choose your license
     license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3'
+    ],
 
     # What does your project relate to?
     keywords='edxml openvas',
